fceeb2997fe9a39edc52c99eef6debc2aabd8e29
John Serrano <andrey_201015@hotmail.com>
2020-10-26 16:39:58 -0500

    Add articles ➡ Readme 🚀

--- src/README.md.tpl
+++ src/README.md.tpl
@@ -10,11 +10,7 @@
 
 
 #### ✍ Latest articles I wrote on [johnserrano.co](https://johnserrano.co/):
-- [Introducción a React Context con los Hooks de React](https://johnserrano.co/introduccion-a-react-context-con-los-hooks-de-react/)
-- [Fundamentos de Programación con JavaScript mi primer Ebook](https://johnserrano.co/fundamentos-de-programacion-con-javascript-mi-primer-ebook/)
-- [Aprende a crear rutas con React Router](https://johnserrano.co/aprende-a-crear-rutas-con-react-router/)
-- [Crea un sitio web con GatsbyJS y React - Introducción](https://johnserrano.co/crea-un-sitio-web-con-gatsbyjs-y-react-introduccion/)
-- [Introducción a los Hooks de React](https://johnserrano.co/introduccion-a-los-hooks-de-react/)
+%{{latest_articles}}%
 
 <!--
 **johnsi15/johnsi15** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
---
+++ src/index.js
@@ -0,0 +1,34 @@
+// Node version > 14 and add "type": "module", package.json
+import { promises as fs } from 'fs'
+// import fetch from 'node-fetch'
+import Parser from 'rss-parser'
+
+const parser = new Parser()
+
+const getLatestArticlesFromBlog = () =>
+  parser
+    .parseURL(
+      'https://admin.johnserrano.co/5c4fe0e52a3a17b604e6eefa245e58/rss/'
+    )
+    .then((data) => data.items)
+
+;(async () => {
+  const [template, articles] = await Promise.all([
+    fs.readFile('./src/README.md.tpl', { encoding: 'utf-8' }),
+    getLatestArticlesFromBlog(),
+  ])
+
+  // create latest articles markdown
+  const latestArticlesMarkdown = articles
+    .slice(0, 5)
+    .map(({ title, link }) => `- [${title}](${link.replace('admin.', '')})`)
+    .join('\n')
+
+  // replace all placeholders with info
+  const newMarkdown = template.replace(
+    '%{{latest_articles}}%',
+    latestArticlesMarkdown
+  )
+
+  await fs.writeFile('README.md', newMarkdown)
+})()
